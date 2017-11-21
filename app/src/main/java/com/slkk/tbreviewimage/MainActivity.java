345b48bd5ee4da1bd7cb222a19be29e873bd7ce0
Your Name <644187060@qq.com>
2017-11-21 16:40:34 +0800

    format the code

--- app/src/main/java/com/slkk/tbreviewimage/MainActivity.java
+++ app/src/main/java/com/slkk/tbreviewimage/MainActivity.java
@@ -38,6 +38,25 @@ import retrofit2.adapter.rxjava2.RxJava2CallAdapterFactory;
 import retrofit2.converter.gson.GsonConverterFactory;
 
 public class MainActivity extends AppCompatActivity {
+    private String auctionNumId = "38088752805";
+    private String userNumId = "775431765";
+    private String currentPageNum = "2";
+    private String pageSize = "20";
+    private String rateType = "3";
+    private String orderType = "sort_weight";
+    private String attribute = "";
+    private String sku = "";
+    private String hasSku = "false";
+    private String folded = "0";
+    private String ua = "098%23E1hv99vWvRhvU9CkvvvvvjiPPLsytj3vn2zwAjljPmPv1jt" +
+            "UPsLU1jYRPs5wgjYnRLurvpvEvvAk9k6rvCpG3QhvCvvhvvm5vpvhvvmv99yCvhQUZ" +
+            "YyvCz7t9ExrV8tYVVz9afmAdcwuYExrQ8g78Boxfa1l5t1zpWsIlnQfV0Q4S47B9Ck" +
+            "aU6bnDO2yjLyDZacEKOm65kx%2FljcGzCuOKphv8vvvvvCvpvvvvvmm86CvmPZvvUU" +
+            "dphvWvvvv9krvpv3Fvvmm86CvmVREvpvVmvvC9jXhuphvmvvv98%2FRsuStvphvC9v" +
+            "hvvCvpvGCvvLMMQvvRphvCvvvvvmCvpvW7D%2B6QVLw7Di4Ij5NRphvCvvvvvv%3D";
+    private String _ksTS = "1510993902912_2186";
+    private String callback = "jsonp_tbcrate_reviews_list";
+
     Gson gson = new GsonBuilder()
             .setLenient()
             .create();
@@ -49,10 +68,10 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        loadData();
+//        loadData();
 
 
-//        downloadImage();
+        downloadImage();
     }
 
     private void loadData() {
@@ -61,21 +80,11 @@ public class MainActivity extends AppCompatActivity {
                 .client(new OkHttpClient())
                 .baseUrl("https://rate.taobao.com/")
                 .build();
+
         TbApi tbApi = retrofit.create(TbApi.class);
-        Call<ResponseBody> imageUrlString = tbApi.getImageUrlString("38088752805",
-                "775431765",
-                "5",
-                "20",
-                "3",
-                "sort_weight",
-                "",
-                "",
-                "false",
-                "0",
-                "098%23E1hv99vWvRhvU9CkvvvvvjiPPLsytj3vn2zwAjljPmPv1jtUPsLU1jYRPs5wgjYnRLurvpvEvvAk9k6rvCpG3QhvCvvhvvm5vpvhvvmv99yCvhQUZYyvCz7t9ExrV8tYVVz9afmAdcwuYExrQ8g78Boxfa1l5t1zpWsIlnQfV0Q4S47B9CkaU6bnDO2yjLyDZacEKOm65kx%2FljcGzCuOKphv8vvvvvCvpvvvvvmm86CvmPZvvUUdphvWvvvv9krvpv3Fvvmm86CvmVREvpvVmvvC9jXhuphvmvvv98%2FRsuStvphvC9vhvvCvpvGCvvLMMQvvRphvCvvvvvmCvpvW7D%2B6QVLw7Di4Ij5NRphvCvvvvvv%3D",
-                "1510993902912_2186",
-                "jsonp_tbcrate_reviews_list"
-        );
+        Call<ResponseBody> imageUrlString = tbApi.getImageUrlString(auctionNumId, userNumId,
+                currentPageNum, pageSize, rateType, orderType,
+                attribute, sku, hasSku, folded, ua, _ksTS, callback);
 
         imageUrlString.enqueue(new Callback<ResponseBody>() {
             @Override
@@ -100,7 +109,10 @@ public class MainActivity extends AppCompatActivity {
                                     @Override
                                     public void run() {
                                         try {
-                                            Bitmap bitmap = Glide.with(getApplicationContext()).asBitmap().load(url2).into(400, 400).get();
+                                            Bitmap bitmap = Glide.with(getApplicationContext())
+                                                    .asBitmap()
+                                                    .load(url2).
+                                                            into(400, 400).get();
                                             saveImage(fileId, bitmap);
                                         } catch (InterruptedException e) {
                                             e.printStackTrace();
@@ -130,7 +142,9 @@ public class MainActivity extends AppCompatActivity {
     }
 
     private void saveImage(long fieldId, Bitmap bitmap) {
-        String absolutePath = Environment.getExternalStoragePublicDirectory(Environment.DIRECTORY_PICTURES).getAbsolutePath();
+        String absolutePath = Environment
+                .getExternalStoragePublicDirectory(Environment.DIRECTORY_PICTURES)
+                .getAbsolutePath();
         Log.i(TAG, "saveImage: " + absolutePath);
         String imgFile = fieldId + ".jpg";
         File file = new File(absolutePath, imgFile);
@@ -146,29 +160,49 @@ public class MainActivity extends AppCompatActivity {
 
     }
 
-//    private void downloadImage() {
-//        Retrofit retrofit = new Retrofit.Builder()
-//                .client(new OkHttpClient())
-//                .baseUrl("https://rate.taobao.com/feedRateList.htm/")
-////                .addConverterFactory(jsonConverter)
-//                .addCallAdapterFactory(rxjavaAdapterFactory)
-//                .build();
-//        TbApi tbApi = retrofit.create(TbApi.class);
-//        tbApi.getImageUrl()
-//                .subscribeOn(Schedulers.io())
-//                .observeOn(Schedulers.io())
-//                .subscribe(new Consumer<JsonRootBean>() {
-//                    @Override
-//                    public void accept(JsonRootBean jsonRootBean) throws Exception {
-//                        List<Comments> comments = jsonRootBean.getComments();
-//                        String promotionType = comments.get(0).getPromotionType();
-//                        Log.i(TAG, "accept: success"+promotionType);
-//                    }
-//                }, new Consumer<Throwable>() {
-//                    @Override
-//                    public void accept(Throwable throwable) throws Exception {
-//                        Log.i(TAG, "accept: exception"+throwable.toString());
-//                    }
-//                });
-//    }
+    private void downloadImage() {
+        Retrofit retrofit = new Retrofit.Builder()
+                .client(new OkHttpClient())
+                .baseUrl("https://rate.taobao.com/")
+                .addConverterFactory(jsonConverter)
+                .addCallAdapterFactory(rxjavaAdapterFactory)
+                .build();
+        TbApi tbApi = retrofit.create(TbApi.class);
+        tbApi.getImageUrl(auctionNumId, userNumId,
+                currentPageNum, pageSize, rateType, orderType,
+                attribute, sku, hasSku, folded, ua, _ksTS, callback)
+                .subscribeOn(Schedulers.io())
+                .observeOn(Schedulers.io())
+                .subscribe(new Consumer<JsonRootBean>() {
+                    @Override
+                    public void accept(JsonRootBean jsonRootBean) throws Exception {
+                        List<Comments> comments = jsonRootBean.getComments();
+                        for (Comments comment : comments) {
+                            List<AppendList> appendList = comment.getAppendList();
+                            for (AppendList append : appendList) {
+                                List<Photos> photos = append.getPhotos();
+                                for (Photos photo : photos) {
+                                    String url = photo.getUrl();
+                                    final String url2 = "http:" + url;
+                                    final long fileId = photo.getFileId();
+                                    Log.i(TAG, "onResponse: " + url);
+                                    Bitmap bitmap = Glide.with(getApplicationContext())
+                                            .asBitmap()
+                                            .load(url2)
+                                            .into(400, 400)
+                                            .get();
+                                    saveImage(fileId, bitmap);
+
+                                }
+
+                            }
+                        }
+                    }
+                }, new Consumer<Throwable>() {
+                    @Override
+                    public void accept(Throwable throwable) throws Exception {
+                        Log.i(TAG, "accept: exception" + throwable.toString());
+                    }
+                });
+    }
 }
--- app/src/main/java/com/slkk/tbreviewimage/api/TbApi.java
+++ app/src/main/java/com/slkk/tbreviewimage/api/TbApi.java
@@ -31,8 +31,20 @@ public interface TbApi {
                                          @Query("callback") String callback
     );
 
-//    @GET("?auctionNumId=38088752805&userNumId=775431765&currentPageNum=1&pageSize=20&rateType=3&orderType=sort_weight&attribute=&sku=&hasSku=false&folded=0&ua=098%23E1hv99vWvRhvU9CkvvvvvjiPPLsytj3vn2zwAjljPmPv1jtUPsLU1jYRPs5wgjYnRLurvpvEvvAk9k6rvCpG3QhvCvvhvvm5vpvhvvmv99yCvhQUZYyvCz7t9ExrV8tYVVz9afmAdcwuYExrQ8g78Boxfa1l5t1zpWsIlnQfV0Q4S47B9CkaU6bnDO2yjLyDZacEKOm65kx%2FljcGzCuOKphv8vvvvvCvpvvvvvmm86CvmPZvvUUdphvWvvvv9krvpv3Fvvmm86CvmVREvpvVmvvC9jXhuphvmvvv98%2FRsuStvphvC9vhvvCvpvGCvvLMMQvvRphvCvvvvvmCvpvW7D%2B6QVLw7Di4Ij5NRphvCvvvvvv%3D&_ksTS=1510993902912_2186&callback=jsonp_tbcrate_reviews_list")
-//    Observable<JsonRootBean> getImageUrl();
+    @GET("feedRateList.htm")
+    Observable<JsonRootBean> getImageUrl(@Query("auctionNumId") String auctionNumId,
+                                         @Query("userNumId") String userNumId,
+                                         @Query("currentPageNum") String currentPageNum,
+                                         @Query("pageSize") String pageSize,
+                                         @Query("rateType") String rateType,
+                                         @Query("orderType") String orderType,
+                                         @Query("attribute") String attribute,
+                                         @Query("sku") String sku,
+                                         @Query("hasSku") String hasSku,
+                                         @Query("folded") String folded,
+                                         @Query("ua") String ua,
+                                         @Query("_ksTS") String ksTs,
+                                         @Query("callback") String callback);
 
 
 }
